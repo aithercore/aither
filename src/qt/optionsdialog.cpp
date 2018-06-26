fe48caad63d5663eb976afc58e0d5eafc567a1fe
flus38765 <flus38765@gmail.com>
2018-06-26 22:06:49 +0700

    fix tx url

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -125,8 +125,6 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
         }
     }
 #if QT_VERSION >= 0x040700
-    QString txUrls = "https://explorer.aithercoin.com/tx/%s";
-    ui->thirdPartyTxUrls->text = txUrls;
     ui->thirdPartyTxUrls->setPlaceholderText("https://example.com/tx/%s");
 #endif
 
@@ -143,7 +141,7 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
     connect(ui->proxyIp, SIGNAL(validationDidChange(QValidatedLineEdit *)), this, SLOT(updateProxyValidationState()));
     connect(ui->proxyIpTor, SIGNAL(validationDidChange(QValidatedLineEdit *)), this, SLOT(updateProxyValidationState()));
     connect(ui->proxyPort, SIGNAL(textChanged(const QString&)), this, SLOT(updateProxyValidationState()));
-    connect(ui->proxyPortTor, SIGNAL(textChanged(const QString&)), this, SLOT(updateProxyValidationState()));
+    connect(ui->proxyPortTor, SIGNAL(textChanged(QString("https://explorer.aithercoin.com/tx/%s"))), this, SLOT(updateProxyValidationState()));
 }
 
 OptionsDialog::~OptionsDialog()
